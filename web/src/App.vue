d9b4dc6dca6a3a937a4e4b60d9048acc6f16ff50
xosvip <51826229+xosvip@users.noreply.github.com>
2023-02-17 16:10:36 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -67,7 +67,10 @@ onMounted(async () => {
     <header id="header" class="bg-dark-50 text-white h-10 select-none">
       <LoadingOutlined v-if="loadding" class="pl-3 cursor-pointer" />
       <span class="text-size-5 pl-5">ChatGPT</span>
-      <span <a href="https://ka.accc.pro" target="_blank">购买账号</a></span>
+  <div @click="goPage('https://ka.accc.pro')">账号购买</div>
+      goPage (url){
+              window.location.href = url
+}
       <a-tooltip>
         <template #title>清除聊天记录</template>
         <a-popconfirm title="确定清除本地所有聊天记录吗?" ok-text="是的" cancel-text="再想想" @confirm="clearMessages">
